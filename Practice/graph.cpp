968ae284f79a57c084cefd6f8f74f60f85218fe5
Oziach <119654408+Oziach@users.noreply.github.com>
2024-07-09 03:04:46 +0530

    graph implementation

---
+++ Practice/graph.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<list>
+#include<unordered_map>
+using namespace std;
+
+class Graph{
+ public:
+    unordered_map<int, list<int>> adj;
+    
+    void AddEdge(int u, int v, bool directed){
+        
+    }
+    
+    void PrintGraph(){
+        
+    }
+};
+
+int main()
+{
+    return 0;
+}
