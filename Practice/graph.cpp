908b95b6ef816195af54f7ab529a797d30dea26d
Oziach <119654408+Oziach@users.noreply.github.com>
2024-07-09 14:27:45 +0530

    graph practice

--- Practice/graph.cpp
+++ Practice/graph.cpp
@@ -9,14 +9,29 @@ class Graph{
     
     void AddEdge(int u, int v, bool directed){
         
+        adj[u].push_back(v);
+        if(!directed){
+            adj[v].push_back(u);
+        }
+        cout << endl;
     }
     
     void PrintGraph(){
-        
+        for(auto p : adj){
+            cout << p.first << " -> ";
+            for(auto node : p.second){
+                cout << node << " ";
+            }
+            cout << endl;
+        }
     }
 };
 
 int main()
 {
+    Graph graph;
+    graph.AddEdge(1,2, false);
+    
+    graph.PrintGraph();
     return 0;
 }
