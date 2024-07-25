0209136e2e9d2d2eabeb1b128d73b9eceb79d3b9
Oziach <bankheleishan@gmail.com>
2024-07-25 21:06:04 +0530

    gas stations problem, binary search on floats/doubles

--- Code360/minimizeMaxDistanceToGasStation.cpp
+++ Code360/minimizeMaxDistanceToGasStation.cpp
@@ -22,3 +22,46 @@ double minimiseMaxDistance(vector<int> &arr, int k){
 
 	return maxHeap.top().first;
 }
+
+//using binary search:
+int GetCount(vector<int>& arr, double dist){
+	
+	int count = 0;
+
+	for(int i = 1; i < arr.size(); i++){
+
+		int noInBetween = ((arr[i] - arr[i-1]) / dist);
+
+		if(arr[i] - arr[i-1] / dist == noInBetween){
+			noInBetween--;
+		}
+		count += noInBetween;
+	}
+
+	return count;
+}
+
+long double minimiseMaxDistance(vector<int> &arr, int k){
+	
+	long double s = 0;
+	long double e = 0;
+
+	for(int i = 0; i < arr.size() - 1; i++){
+		e = max(e, (long double)(arr[i+1] - arr[i]));
+	}
+
+	long double diff = 1e-6;
+	while(e - s > diff){
+		long double mid = (s + e)/2.0;
+		int numberOfStations = GetCount(arr, mid);
+
+		if(numberOfStations > k){
+			s = mid;
+		}
+		else{
+			e = mid;
+		}
+	}
+
+	return e;
+}
